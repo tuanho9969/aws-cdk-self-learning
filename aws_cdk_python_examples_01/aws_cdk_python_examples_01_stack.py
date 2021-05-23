d8826c58f52f410f6e135450a58dce4a088bc201
EC2 Default User <ec2-user@ip-10-10-1-12.ap-southeast-1.compute.internal>
2021-05-23 08:17:44 +0000

    httpd on asg with alb front

--- aws_cdk_python_examples_01/aws_cdk_python_examples_01_stack.py
+++ aws_cdk_python_examples_01/aws_cdk_python_examples_01_stack.py
@@ -1,5 +1,7 @@
 from aws_cdk import (
     aws_ec2 as ec2,
+    aws_autoscaling as asg,
+    aws_elasticloadbalancingv2 as elb,
     core
     )
 
@@ -16,23 +18,51 @@ class CDKExample01Stack(core.Stack):
         httpd = ec2.UserData.for_linux()
         httpd.add_commands(str(data,'utf-8'))
         
-        instance = ec2.Instance(
-            self, "MyFirstCDKInstance",
+        _asg = asg.AutoScalingGroup(
+            self,
+            "ASG",
             vpc=vpc,
-            vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PUBLIC),
             instance_type=ec2.InstanceType(instance_type_identifier="t2.large"),
             machine_image=ec2.AmazonLinuxImage(
                 generation=ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
                 storage=ec2.AmazonLinuxStorage.GENERAL_PURPOSE,
-            ),
-            instance_name="MyFirstCDKEC2Instance",
-            #user_data=ec2.UserData.add_execute_file_command(self, file_path="./user_data.sh")
+                ),
             user_data=httpd,
+            vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PUBLIC),
+            desired_capacity=2,
+            auto_scaling_group_name="FirstASG"
+        )
+        
+        _asg.scale_on_cpu_utilization("ScaleOnCPU_Utilization",target_utilization_percent=50)
+        
+        alb = elb.ApplicationLoadBalancer(
+            self,
+            "ALB",
+            vpc=vpc,
+            vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PUBLIC),
+            load_balancer_name="FirstALB",
+            internet_facing=True
+            )
+        
+        listener = alb.add_listener("Listener", port=80)
+        listener.add_targets("Target", port=80, targets=[_asg])
+        listener.connections.allow_from_any_ipv4(
+            ec2.Port.tcp(80), "Allows HTTP from the World to call"
             )
             
-        instance.connections.allow_from_any_ipv4(
-            ec2.Port.tcp(22), "Internet access SSH"
-            )
-        instance.connections.allow_from_any_ipv4(
-            ec2.Port.tcp(80), "Internet access HTTP"
-            )
+        core.CfnOutput(self,"LoadBalancer",export_name="LoadBalancer", value=alb.load_balancer_dns_name)
+        
+        
+        #_asg.connections.allow_from_any_ipv4(
+        #    ec2.Port.tcp(22), "Internet access SSH"
+        #   )
+        #_asg.connections.allow_from_any_ipv4(
+        #    ec2.Port.tcp(80), "Internet access HTTP"
+        #    )
+            
+        #instance.connections.allow_from_any_ipv4(
+        #    ec2.Port.tcp(22), "Internet access SSH"
+        #    )
+        #instance.connections.allow_from_any_ipv4(
+        #    ec2.Port.tcp(80), "Internet access HTTP"
+        #    )
